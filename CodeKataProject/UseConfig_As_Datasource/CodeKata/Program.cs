134aa9a6a1154e8e15be5b6c6fc3643caa47d367
CORPAA\919842 <yelena.gouralnik@aa.com>
2019-11-08 16:46:05 -0700

    Initial Commit: Add libraries, add initialization of the configuration file, add documentation

---
+++ CodeKataProject/UseConfig_As_Datasource/CodeKata/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace CodeKata
+{
+    class Program
+    {
+        /// <summary>
+        /// Initialize configuration builder for a console application
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+          var builder = new ConfigurationBuilder()
+         .SetBasePath(Directory.GetCurrentDirectory())
+         .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+
+            Console.WriteLine(configuration.GetConnectionString("Storage"));
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # CodeKataDotnet
-Code Ka
+Code Kata
+Install 
+Microsoft.Extensions.Configuration
+Microsoft.Extensions.Configuration.FileExtensions
+Install-package Microsoft.Extensions.Configuration.Json
+
+For documentation open the .csprj file and add
+   <DocumentationFile>bin\Debug\netcoreapp3.0\App.xml</DocumentationFile>
+   This is where your documentation is going to be generated
+
