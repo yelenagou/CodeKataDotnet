910c9ecae07b18786a9231c5d5b1fba953c09765
CORPAA\919842 <yelena.gouralnik@aa.com>
2020-01-02 23:39:22 -0700

    c# 8 null handling coalescing

---
+++ CodeKataProject/CodeKata/CS8NullsHandling/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using HandlingNulls.DataModel;
+using System;
+
+namespace CS8NullsHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Message message = new Message
+            {
+                Text = "Hello There",
+                From = null
+            };
+            //string messageString = null;
+            //null coalescing
+            Console.WriteLine($"From: {message.From ?? "No from"} Message: {message.Text} {message.ToUpperFrom()}");
+            Console.WriteLine("Press Enter To End");
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ CodeKataProject/CodeKata/HandlingNulls/DataModel/Message.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HandlingNulls.DataModel
+{
+    public class Message
+    {
+        public string? From { get; set; }
+        public string Text { get; set; } = "";
+
+        //public string ToUpperFrom()
+        //{
+        //    if(From is null)
+        //    {
+        //        return "";
+        //    }
+        //    return From.ToUpperInvariant();
+
+        //}
+        public string ToUpperFrom() => From?.ToUpperInvariant();
+            
+    }
+}
